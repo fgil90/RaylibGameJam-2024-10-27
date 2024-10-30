44847a71225648ef497522478198c60f50ba5fa1
Felipe Gil <fgil90@gmail.com>
2024-10-30 00:33:21 +0000

    Enemies and enemy tracking
    Add Enemies
    Drones now face enemies

--- src/raylib_game.c
+++ src/raylib_game.c
@@ -36,7 +36,7 @@
 #endif
 
 #define LOGO_DISPLAY_FRAMES 120
-#define MAX_DRONES 2
+#define MAX_DRONES 10
 #define MAX_ENEMIES 100
 
 
@@ -67,6 +67,8 @@ typedef struct Module {
 };
 
 typedef struct Drone {
+    bool alive;
+
     Vector2 position;
     Vector2 velocity;
     Vector2 acceleration;
@@ -74,17 +76,24 @@ typedef struct Drone {
     float maxAccel;
     float maxVelocity;
     float jerk;
-    int size;
 
-    Vector2 shotDirection;
+    int size;
+    Color color;
+    float playerMinDistance;
+    float playerMaxDistance;
+
     int framesSinceShotFired;
     int shotCooldownFrames;
+    float detectRange;
+    float facingAngle;
     bool canShoot;
 
 }Drone;
 
 typedef struct Enemy {
+    bool alive;
     Vector2 position;
+    int size; 
 }Enemy;
 
 typedef struct Player {
@@ -118,10 +127,15 @@ static RenderTexture2D target = { 0 };  // Render texture to render our game
 
 // TODO: Define global variables here, recommended to make them static
 
+void UpdateDrawDrone(Drone* drone);
+
 //----------------------------------------------------------------------------------
 // Module Functions Declaration
 //----------------------------------------------------------------------------------
 static void UpdateDrawFrame(void);      // Update and Draw one frame
+void InitPlayer();
+void InitDrones();
+void InitEnemies();
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -136,46 +150,9 @@ int main(void)
     //--------------------------------------------------------------------------------------
     InitWindow(screenWidth, screenHeight, "TBD Game Name");
 
-    
-
-    // Init Player 
-    player.position = (Vector2){ 384.0f , 400.0f };
-    player.size = 32;
-    player.acceleration = 2500.0f;
-    player.maxVelocity = 650.0f;
-    player.dampening = 0.90f;
-    player.hitbox = (Rectangle){
-        player.position.x,
-        player.position.y,
-        player.position.x + player.size,
-        player.position.y + player.size
-    };
-
-    // Init Drones
-    player.drones[0] = (Drone){
-        .position = player.position,
-        .jerk = 5000.0f,
-        .maxAccel = 2000.0f,
-        .maxVelocity = 700.0f,
-        .dampening = 0.90f,
-        .framesSinceShotFired = 0,
-        .shotCooldownFrames = 60,
-        .size = 20,
-    };
-
-    player.drones[1] = (Drone){
-        .position = Vector2Add(player.position, (Vector2) { 20.0f, 20.f }),
-        .jerk = 5000.0f,
-        .maxAccel = 2000.0f,
-        .maxVelocity = 700.0f,
-        .dampening = 0.90f,
-        .framesSinceShotFired = 0,
-        .shotCooldownFrames = 60,
-        .size = 20,
-
-    };
-    
-    // TODO: Load resources / Initialize variables at this point
+    InitPlayer();
+    InitDrones();
+    InitEnemies();
     
     // Render texture to draw full screen, enables screen scaling
     // NOTE: If screen is scaled, mouse input should be scaled proportionally
@@ -210,6 +187,81 @@ int main(void)
 //--------------------------------------------------------------------------------------------
 // Module functions definition
 //--------------------------------------------------------------------------------------------
+
+void InitPlayer() {
+    // Init Player 
+    player.position = (Vector2){ 384.0f , 400.0f };
+    player.size = 32;
+    player.acceleration = 2500.0f;
+    player.maxVelocity = 650.0f;
+    player.dampening = 0.90f;
+    player.hitbox = (Rectangle){
+        player.position.x,
+        player.position.y,
+        player.position.x + player.size,
+        player.position.y + player.size
+    };
+}
+
+void InitDrones(){
+    // Init Drones
+
+    //Init dead drones
+    for (size_t i = 0; i < MAX_DRONES; i++)
+    {
+        player.drones[i] = (Drone){
+            .alive = false,
+        };
+    }
+
+    // Test Drones
+    player.drones[0] = (Drone){
+        .alive = true,
+        .position = player.position,
+        .jerk = 5000.0f,
+        .maxAccel = 2000.0f,
+        .maxVelocity = 700.0f,
+        .dampening = 0.90f,
+        .framesSinceShotFired = 0,
+        .shotCooldownFrames = 60,
+        .size = 20,
+        .color = BLUE,
+        .playerMaxDistance = 80.0f,
+        .playerMinDistance = 40.0f,
+        .detectRange = 100.0f,
+        .facingAngle = 0.0f,
+    };
+
+    player.drones[1] = (Drone){
+        .alive = true,
+        .position = Vector2Add(player.position, (Vector2) { 20.0f, 20.f }),
+        .jerk = 5000.0f,
+        .maxAccel = 2000.0f,
+        .maxVelocity = 700.0f,
+        .dampening = 0.90f,
+        .framesSinceShotFired = 0,
+        .shotCooldownFrames = 60,
+        .size = 20,
+        .color = PINK,
+        .playerMaxDistance = 80.0f,
+        .playerMinDistance = 40.0f,
+        .detectRange = 100.0f,
+        .facingAngle = 0.0f,
+    };
+}
+
+void InitEnemies() {
+    for (size_t i = 0; i < MAX_ENEMIES; i++)
+    {
+        enemyArray[i].alive = false;
+    }
+    for (size_t i = 0; i < 10; i++)
+    {
+        enemyArray[i].alive = true;
+        enemyArray[i].position = (Vector2){ 200 + i * 40, 200 };
+        enemyArray[i].size = 16;
+    }
+}
 
 void DrawLogoScreen() {
     BeginTextureMode(target);
@@ -276,13 +328,11 @@ void DrawDebug() {
     Vector2 playerCenter = CalcCenter(player.position, player.size);
     DrawLineV(playerCenter, Vector2Add(playerCenter, Vector2Scale(player.velocity, 1)), GREEN);
 
-    DrawText(TextFormat("%f", Vector2Length(player.drones[0].acceleration)), player.position.x - 10, player.position.y - 10, 14, WHITE);
+    //DrawText(TextFormat("%f", Vector2Length(player.drones[0].acceleration)), player.position.x - 10, player.position.y - 10, 14, WHITE);
 }
 
 void MoveDrone(Drone* drone) {
     float delta = GetFrameTime();
-    float playerMinDistance = 40.0f;
-    float playerMaxDistance = 80.0f;
 
     Vector2 playerCenter = CalcCenter(player.position, player.size);
     Vector2 droneCenter = drone->position;
@@ -292,11 +342,11 @@ void MoveDrone(Drone* drone) {
     Vector2 playerToDrone = Vector2Normalize(Vector2Subtract(droneCenter, playerCenter));
     Vector2 normalizedVelocity = Vector2Normalize(drone->velocity);
 
-    if (playerDroneDistance > playerMaxDistance) {
-        drone->acceleration = Vector2Add(drone->acceleration, Vector2Scale(droneToPlayer, drone->jerk * delta));
-        drone->velocity = Vector2Scale(drone->velocity, Vector2DotProduct(droneToPlayer, normalizedVelocity));
+    if (playerDroneDistance > drone->playerMaxDistance) {
+        drone->acceleration = Vector2Add(drone->acceleration, Vector2Scale(droneToPlayer, drone->jerk/100 * playerDroneDistance * delta));
+        drone->velocity = Vector2Scale(drone->velocity, Vector2DotProduct(droneToPlayer, normalizedVelocity)*0.2f + 0.8f);
     }
-    else if (playerDroneDistance < playerMinDistance) {
+    else if (playerDroneDistance < drone->playerMinDistance) {
         drone->acceleration = Vector2Add(drone->acceleration, Vector2Scale(playerToDrone, drone->jerk * delta));
     }
     else {
@@ -310,7 +360,7 @@ void MoveDrone(Drone* drone) {
 }
 
 void DrawDrone(Drone* drone) {
-    DrawCircleV(drone->position, drone->size, BLUE);
+    DrawCircleV(drone->position, drone->size, drone->color);
 }
 
 void DrawDroneConnection(Drone* drone) {
@@ -319,35 +369,68 @@ void DrawDroneConnection(Drone* drone) {
     DrawLineV(playerCenter, droneCenter, YELLOW);
 }
 
-Vector2 CalculateShotDirection(Drone* drone, Enemy* enemy) {
-
+Vector2 FindEnemyDirection(Drone* drone, Enemy* enemy) {
+    return Vector2Normalize(Vector2Subtract(enemy->position, drone->position));
 }
 
 void Shoot(Drone* drone, Vector2 direction) {
-
+    
 }
 
-Enemy* ClosestEnemyInRange(Drone* drone, Enemy* enemyArray) {
-    return NULL;
+Enemy* FindClosestEnemyInRange(Drone* drone, float range) {
+    Enemy* closest = NULL;
+    float distanceToClosest = INT_MAX;
+    for (size_t i = 0; i < MAX_ENEMIES; i++)
+    {
+        if (!enemyArray[i].alive) continue;
+
+        float distanceToEnemy = Vector2Distance(enemyArray[i].position, drone->position);
+        if (distanceToEnemy > range) continue;
+        if (distanceToEnemy > distanceToClosest) continue;
+
+        closest = &enemyArray[i];
+        distanceToClosest = distanceToEnemy;
+    }
+    return closest;
+}
+
+void DrawDroneFacingDirection(Drone* drone, float arcsize) {
+    if (arcsize < 20) arcsize = 20;
+    DrawCircleSector(drone->position, drone->size + 3.0f, drone->facingAngle*180/PI - arcsize / 2, drone->facingAngle*180 / PI + arcsize / 2, 1, GREEN);
+}
+
+void TurnTowardsDirection(Drone* drone, Vector2 direction) {
+    float delta = GetFrameTime();
+    float directionAngle = Vector2Angle((Vector2) { 1.0f, 0.0f }, direction);
+    float turningFactor = 0.1f; // [0.0f - 1.0f] Higher means faster turning towards enemy.
+    drone->facingAngle = Lerp(drone->facingAngle, directionAngle, turningFactor);
 }
 
 void UpdateDrawDrone(Drone* drone) {
-
     MoveDrone(drone);
 
+    DrawDroneFacingDirection(drone, 20);
     DrawDrone(drone);
     DrawDroneConnection(drone);
+
+    drone->framesSinceShotFired++;
     if (drone->framesSinceShotFired > drone->shotCooldownFrames) drone->canShoot = true;
 
     if (!drone->canShoot) return;
-    Enemy* closestEnemy = ClosestEnemyInRange(drone, &enemyArray);
+    Enemy* closestEnemy = FindClosestEnemyInRange(drone, drone->detectRange);
     if (closestEnemy == NULL) return;
+    //DrawCircleV(closestEnemy->position, 10.0f, drone->color); // Debug Closest Enemy in Range
+    DrawLineV(drone->position, closestEnemy->position, WHITE); // Debug Closest Enemy in Range
     
-    Vector2 shotDirection = CalculateShotDirection(drone, &closestEnemy);
-    Shoot(drone, shotDirection);
+    Vector2 enemyDirection = FindEnemyDirection(drone, closestEnemy);
+    TurnTowardsDirection(drone, enemyDirection);
+    Shoot(drone, enemyDirection);
     drone->framesSinceShotFired = 0;
 }
 
+void DrawEnemy(Enemy *enemy) {
+    DrawPolyLines(enemy->position, 6, enemy->size, 0.0f, WHITE);
+}
 
 // Update and draw frame
 void UpdateDrawFrame(void)
@@ -379,9 +462,30 @@ void UpdateDrawFrame(void)
             BeginDrawing();
                 ClearBackground(BLACK);
                 DrawShipHitbox();
+
+                // Drone Logic and Draw
                 for (size_t i = 0; i < MAX_DRONES; i++)
                 {
+                    if (!player.drones[i].alive) continue;
+
                     UpdateDrawDrone(&player.drones[i]);
+                    // Nudge Drones away from each other
+                    for (size_t j = 0; j < MAX_DRONES; j++)
+                    {
+                        if (i == j) continue;
+                        float nudgeRadius = (float)player.drones[i].size*2 + 4;
+                        float distJtoI = Vector2Distance(player.drones[i].position, player.drones[j].position);
+                        if (distJtoI > nudgeRadius) continue;
+                        Vector2 dirJtoI = Vector2Normalize(Vector2Subtract(player.drones[i].position, player.drones[j].position));
+                        player.drones[i].acceleration = Vector2Add(player.drones[i].acceleration, Vector2Scale(dirJtoI, 30.0f));
+                    }
+                }
+
+                // Enemy Logic and Draw
+                for (size_t i = 0; i < MAX_ENEMIES; i++)
+                {   
+                    if (!enemyArray[i].alive) continue;
+                    DrawEnemy(&enemyArray[i]);
                 }
                 DrawDebug();
             EndDrawing();
